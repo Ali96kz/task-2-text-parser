b8cfc32eede6063b95e34d704723040ad4a8b299
Ali96kz <jakparov.kz@gmail.com>
2016-08-02 00:56:34 +0600

    add logger

--- src/main/java/com/epam/az/text/parser/Main.java
+++ src/main/java/com/epam/az/text/parser/Main.java
@@ -1,11 +1,20 @@
 package com.epam.az.text.parser;
 
+import com.epam.az.text.parser.entity.Text;
+import com.epam.az.text.parser.parser.GenericRegExParser;
+import com.epam.az.text.parser.parser.Parser;
+import com.epam.az.text.parser.service.FileReaderz;
+import com.epam.az.text.parser.service.Finder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 
 public class Main {
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException {
-    /*    FileReaderz fileReader = new FileReaderz();
+        FileReaderz fileReader = new FileReaderz();
         String s = fileReader.read();
         Parser parser = new GenericRegExParser();
         Text text = parser.parse(Text.class, s);
@@ -13,8 +22,6 @@ public class Main {
         Finder finder = new Finder();
         StringBuilder stringBuilder = new StringBuilder();
         finder.deleteWord(text, stringBuilder);
-        System.out.println(stringBuilder);
-    */
-
+        logger.info(stringBuilder.toString());
     }
 }
